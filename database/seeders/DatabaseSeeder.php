ba74a65d1b04c74ce6b9d93743d3f44924d73051
David <da.b@hotmail.es>
2024-07-10 12:23:58 +0200

    setup models, migrations and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,10 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        // Creamos un usuario
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Dab User',
+            'email' => 'da.b@hotmail.es',
+            'password' => bcrypt('123.324A'),
+            'email_verified_at' => time()
         ]);
+
+        Project::factory()->count(30)->hasTasks(30)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    // Inertia es la que renderiza los componentes y rutas (resources/js/pages)
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
